ce260c902d722a4509fe29edd3522d1455679f07
tsa <tuomo.saarikivi@outlook.com>
2016-07-31 23:28:09 +0300

    ccc

--- public/index.js
+++ public/index.js
@@ -265,17 +265,64 @@ module.exports =
 	//######################################################
 
 	  JPS.app.get('/paytrailnotification', (req, res) => {
-	    var merchantAuthenticationhash = "6pKF4jkv97zmqBJ3ZL8gUw5DfT2NMQ"
+	    JPS.merchantAuthenticationhash = "6pKF4jkv97zmqBJ3ZL8gUw5DfT2NMQ"
 	    console.log("paytrailnotification requested");
 	    console.log("ORDER_NUMBER", req.query.ORDER_NUMBER);
 	    console.log("TIMESTAMP", req.query.TIMESTAMP);
 	    console.log("PAID", req.query.PAID);
 	    console.log("METHOD", req.query.METHOD);
 	    console.log("RETURN_AUTHCODE", req.query.RETURN_AUTHCODE);
-	    var hash1 = md5(req.query.ORDER_NUMBER + '|' + req.query.TIMESTAMP + '|' + req.query.PAID + '|' + req.query.METHOD + '|' + merchantAuthenticationhash).toUpperCase()
-	    var hash2 = md5(req.query.ORDER_NUMBER + '|' + req.query.TIMESTAMP + '|' + merchantAuthenticationhash).toUpperCase()
-	    console.log("HASH1", hash1);
-	    console.log("HASH2", hash2);
+	    JPS.hashOK = md5(req.query.ORDER_NUMBER + '|' + req.query.TIMESTAMP + '|' + req.query.PAID + '|' + req.query.METHOD + '|' + merchantAuthenticationhash).toUpperCase()
+	    JPS.hashNOK = md5(req.query.ORDER_NUMBER + '|' + req.query.TIMESTAMP + '|' + merchantAuthenticationhash).toUpperCase()
+	    console.log("HASH-OK", JPS.hashOK);
+	    console.log("HASH-NOK", JPS.hashNOK);
+	    JPS.orderNumber = req.query.ORDER_NUMBER;
+	    JPS.timeStamp = req.query.TIMESTAMP;
+	    JPS.paymentTransactionRef = req.query.PAID;
+	    JPS.paymentMethod = req.query.METHOD
+	    JPS.authorizationCode = req.query.RETURN_AUTHCODE;
+	    if(req.query.PAID){
+	      if(hash1 === req.query.RETURN_AUTHCODE){
+	////////////////
+	        JPS.firebase.database().ref('/pendingtransactions/'+JPS.orderNumber).once('value')
+	        .then(snapshot => {
+	          JPS.pendingTransaction = snapshot.val()
+	          console.log("Processing pending transaction: ", JPS.pendingTransaction)
+	          return JPS.firebase.database().ref('/transactions/'+JPS.pendingTransaction.user+'/'+JPS.pendingTransaction.timestamp)
+	          .update(Object.assign(
+	            JPS.pendingTransaction.transaction, 
+	            JPS.pendingTransaction.shopItem, {
+	            details: {
+	              success: true,
+	              transaction: {
+	                amount: JPS.pendingTransaction.shopItem.price,
+	                currencyIsoCode: "EUR",
+	                id: JPS.paymentTransactionRef,
+	                paymentInstrumentType: "PayTrail",
+	                paymentMethod: JPS.paymentMethod 
+	              }
+	            }
+	          }))
+	        })
+	        .then(() => {
+	          console.log("Pending transaction processed succesfully. Removing pending record.");
+	          JPS.firebase.database().ref('/pendingtransactions/'+JPS.orderNumber).remove();
+	        })
+	        .catch(error => {
+	          console.error("Processing pendingtransactions failed: ", JPS.orderNumber, error);
+	          throw(new Error("Processing pendingtransactions failed: " + JPS.orderNumber + error.message))
+	        })
+	////////////////
+	      } else {
+	        throw (new Error("Input authorization code did not match: " + JPS.hashOK + "!=" + JPS.authorizationCode + " --- " + JPS.hashNOK))
+	      }
+	    }
+	    else{
+	      JPS.firebase.database().ref('/pendingtransactions/'+JPS.orderNumber).remove().catch(error => {
+	        console.error("Removing pending transaction failed.");
+	        throw (new Error("Removing pending transaction failed." + error.message))
+	      })
+	    }
 	    res.status(200).end();
 	  })
 	}
