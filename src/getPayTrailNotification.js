d7a9b1f9dc96d4084c92979f08fbf8eca73adcfc
tsa <tuomo.saarikivi@outlook.com>
2016-07-31 19:43:29 +0300

    x

--- src/getPayTrailNotification.js
+++ src/getPayTrailNotification.js
@@ -6,7 +6,12 @@ exports.setApp = function (JPS){
 //######################################################
 
   JPS.app.get('/paytrailnotification', (req, res) => {
-    console.log("paytrailnotification requested", req, res);
+    console.log("paytrailnotification requested");
+    console.log("ORDER_NUMBER", req.query.ORDER_NUMBER);
+    console.log("TIMESTAMP", req.query.TIMESTAMP);
+    console.log("PAID", req.query.PAID);
+    console.log("METHOD", req.query.METHOD);
+    console.log("RETURN_AUTHCODE", req.query.RETURN_AUTHCODE);
     res.status(200).end();
   })
 }
